c765c158951ce99bbe45d00c2afdf5bc0d2a2db1
Senpaka <egorkazakov01012007@mail.ru>
2025-11-06 23:22:20 +0300

    добавил функцию вывода суммы предсказаний

--- Stepic/LineRegression/MyLineReg.py
+++ Stepic/LineRegression/MyLineReg.py
@@ -20,7 +20,6 @@ class MyLineReg():
         print(f"{n} | loss: {loss}")
 
     def fit(self, X: pd.DataFrame, y: pd.Series, verbose=False):
-
         X = X.copy()
         X.insert(0, "once", 1)
         self.weight = np.ones(X.shape[1])
@@ -32,11 +31,8 @@ class MyLineReg():
 
         for i in range(self.n_iter):
             Y = X_matrix @ self.weight
-
             loss = Y - Y_vector
-
             grad = 2 / n_samples * (X_matrix.T @ loss)
-
             self.weight = self.weight - self.learning_rate * grad
 
             if verbose and i % verbose == 0:
@@ -45,6 +41,15 @@ class MyLineReg():
     def get_coef(self):
         return self.weight[1:]
 
+    def predict(self, X: pd.DataFrame):
+
+        X = X.copy()
+        X.insert(0, "ones", 1)
+        pred = X @ self.weight
+
+        return pred.sum()
+
 linReg = MyLineReg(50, 0.1)
 linReg.fit(X, y, 10)
 print(linReg.get_coef())
+print(linReg.predict(X))
